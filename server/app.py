3093cbdccbc8044b14a4f746d12c0f77fa82d152
Wing <wingyeung0317@hotmail.com>
2024-04-06 02:14:18 +0800

    Init: create docker files, under development

--- server/app.py
+++ server/app.py
@@ -1,7 +1,24 @@
 from flask import Flask, request
-# from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
 
+import localconst # Save your sql connection data into localconst.py
+user = localconst.user
+password = localconst.password
+hostname = localconst.hostname
+database_name = localconst.database_name
+
+engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{hostname}/{database_name}")
+print(engine)
+
+app = Flask(__name__)
+# CORS(app)
+
+@app.route('/')
+def hello():
+    return 'Hello world!'
+    
+if __name__ == '__main__':
+    app.run()
